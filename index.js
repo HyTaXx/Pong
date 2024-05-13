c457933b1cfd1c9d9bb7442c4394f742eab701b0
HyTaXx <nobrearmand@gmail.com>
2024-05-13 14:41:18 +0200

    :hammer: fixing ball pos

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ io.on("connection", (socket) => {
 
         socket.room = room;
 
-        if (io.sockets.adapter.rooms.get(room).size === 1) {
-            players.push({ id: socket.id, role: "left" });
-            io.to(socket.id).emit("playerRole", "left");
-        } else if (io.sockets.adapter.rooms.get(room).size === 2) {
-            players.push({ id: socket.id, role: "right" });
-            io.to(socket.id).emit("playerRole", "right");
+        if (io.sockets.adapter.rooms.get(room).size === 2) {
+            console.log("Emitting gameStart event");
             io.to(room).emit("gameStart", "Game started");
+            const clients = io.sockets.adapter.rooms.get(room);
+            let players = Array.from(clients);
+            io.to(players[0]).emit("playerRole", "left");
+            io.to(players[1]).emit("playerRole", "right");
         }
 
         console.log("Players: ", players);
@@ -42,6 +42,14 @@ io.on("connection", (socket) => {
         console.log("A user disconnected");
         players = players.filter((player) => player.id !== socket.id);
         console.log("Players: ", players);
+    });
+
+    socket.on("playerName", (name) => {
+        console.log("Player name: ", name);
+    });
+
+    socket.on("ballPosition", (position) => {
+        socket.broadcast.emit("ballPosition", position);
     });
 });
 
